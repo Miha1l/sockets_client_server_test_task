73c652832a7c51d0b1d3368de016e0e1d9af021a
Miha1l <mr.loykonen@mail.ru>
2025-01-17 00:26:31 +0300

    upd build script

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
-cmake -S ./client -B ./client/build
-cmake --build ./client/build/
+#!/bin/bash
 
-cmake -S ./server -B ./server/build
-cmake --build ./server/build/
+build() {
+    CMAKE_DIR=$1
+    BUILD_DIR=$2
+
+    cmake -S $CMAKE_DIR -B $BUILD_DIR
+    cmake --build $BUILD_DIR
+}
+
+if [[ $1 == "client" ]]
+then
+    build ./client ./client/build
+elif [[ $1 == "server" ]]
+then
+    build ./server ./server/build
+else
+    build ./client ./client/build
+    build ./server ./server/build
+fi
